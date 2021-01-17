58551be821bcb2e744a212f8d3d9abd1999c5d03
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-01-17 23:09:43 +0100

    Add Docker setup for server and Client

--- music-earrings-front/src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="App">Music Earrings</div>;
-}
-
-export default App;
--- server/server.js
+++ server/server.js
@@ -3,10 +3,11 @@ var request = require("request"); // "Request" library
 var cors = require("cors");
 var querystring = require("querystring");
 var cookieParser = require("cookie-parser");
+import { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } from "./secrets";
 
-var client_id = "CLIENT_ID"; // Your client id
-var client_secret = "CLIENT_SECRET"; // Your secret
-var redirect_uri = "REDIRECT_URI"; // Your redirect uri
+var client_id = CLIENT_ID; // Your client id
+var client_secret = CLIENT_SECRET; // Your secret
+var redirect_uri = REDIRECT_URI; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
