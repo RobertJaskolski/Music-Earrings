43d7ed222cef00e6f19c7df57529f8ab8e82c25c
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-02-01 21:54:27 +0100

    Added Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { Container, Grid } from "@material-ui/core";
 import Nav from "./containers/Nav/Nav";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Container maxWidth="lg">
+      <Container maxWidth="xl">
         <Nav />
+        <Footer />
       </Container>
     </div>
   );
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,45 @@
+import { Grid } from "@material-ui/core";
+import React from "react";
+import styled from "styled-components";
+
+const FooterStyled = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Line = styled.div`
+  background-color: #1ed760;
+  height: 1px;
+  width: 100%;
+`;
+const Ul = styled.span`
+  list-style-type: none;
+`;
+const Li = styled.li`
+  display: inline-block;
+  margin: 15px;
+`;
+
+export default function Footer() {
+  return (
+    <Grid item xs={12}>
+      <Grid container justify="center">
+        <Grid item xs={8}>
+          <Line></Line>
+        </Grid>
+        <Grid item xs={8}>
+          <FooterStyled>
+            <span>&copy; 2021 Robert Jaskólski</span>
+            <span>
+              <Ul>
+                <Li>Github</Li>
+                <Li>Linkedin</Li>
+                <Li>Contact</Li>
+              </Ul>
+            </span>
+          </FooterStyled>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
