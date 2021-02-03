4a71e20fce724ce2fbf434eb0e862960670a2bbb
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-02-03 18:18:55 +0100

    Create store and create root reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,17 @@
-import React, { useEffect, useState } from "react";
-import queryString from "query-string";
+import React from "react";
 import { Container } from "@material-ui/core";
 import Nav from "./containers/Nav/Nav";
 import Footer from "./components/Footer/Footer";
+import rootReducer from "./reducers";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+const store = createStore(rootReducer, composeWithDevTools());
 
 function App() {
-  const [tokens, setTokens] = useState();
-  const getTokens = () => {
-    const { access_token, refresh_token } = queryString.parse(
-      window.location.hash
-    );
-    return access_token;
-  };
-  useEffect(() => {
-    setTokens(getTokens());
-  });
-
   return (
     <div className='App'>
       <Container maxWidth='xl'>
         <Nav />
-        {tokens}
         <Footer />
       </Container>
     </div>
