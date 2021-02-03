d3ec209e2e55630962029b176ea31795addc5434
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-02-03 19:22:33 +0100

    Separated store, Added react-redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,7 @@ import React from "react";
 import { Container } from "@material-ui/core";
 import Nav from "./containers/Nav/Nav";
 import Footer from "./components/Footer/Footer";
-import rootReducer from "./reducers";
-import { createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { tokensActions } from "./reducers/tokens";
-const store = createStore(rootReducer, composeWithDevTools());
-store.dispatch(
-  tokensActions.refresh({ accessToken: "bla", refreshToken: "la" })
-);
-store.dispatch(tokensActions.clear());
+
 function App() {
   return (
     <div className='App'>
