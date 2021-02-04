8500dd905d75dde65f8e1547bfd5bb65828c35ac
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-02-04 17:16:45 +0100

    Added subscribe store
    Save taokens to localStorage

---
+++ client/src/LocalStorage.js
@@ -0,0 +1,20 @@
+export const loadTokens = () => {
+  try {
+    const serializedTokens = localStorage.getItem("tokens");
+    if (serializedTokens === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedTokens);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+export const saveTokens = (tokens) => {
+  try {
+    const serializedTokens = JSON.stringify(tokens);
+    localStorage.setItem("tokens", serializedTokens);
+  } catch (err) {
+    console.log(err);
+  }
+};
