c1bd50764c0b78626b41581a2aeaa42c52361760
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-02-08 22:20:15 +0100

    Edit Api call

--- client/src/api/SpotifyAPI/UserProfile.js
+++ client/src/api/SpotifyAPI/UserProfile.js
@@ -2,11 +2,16 @@ import instanceAuth, { setToken } from "./Instance";
 
 async function GetUserProfile() {
   setToken();
-  await instanceAuth.get("/v1/me").then((response) => {
-    if (response.statusText === "OK") {
-      return response.data;
-    } else return undefined;
-  });
+  await instanceAuth
+    .get("/v1/me")
+    .then((response) => {
+      if (response.statusText === "OK") {
+        return response.data;
+      } else return undefined;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 export default GetUserProfile;
