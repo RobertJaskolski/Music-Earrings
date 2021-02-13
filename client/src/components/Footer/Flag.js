db378fb087ee48035848c246418848518cb4ebd0
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-02-13 18:42:01 +0100

    Add test for flag

--- client/src/components/Footer/Flag.js
+++ client/src/components/Footer/Flag.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import poland from "../../images/poland.png";
+import PropTypes from "prop-types";
+
 const FlagBox = styled.div`
   width: 30px;
   height: 16px;
@@ -12,10 +14,26 @@ const FlagBox = styled.div`
   }
 `;
 
-export default function Flag() {
+export function Flag(props) {
+  const { lang } = props;
+  if (!lang) {
+    return null;
+  }
   return (
-    <FlagBox>
-      <img alt='lang' width='100%' height='100%' src={poland} />
+    <FlagBox data-test='FlagComponent'>
+      <img
+        alt='lang'
+        width='100%'
+        height='100%'
+        src={poland}
+        data-test='FlagIMG'
+      />
     </FlagBox>
   );
 }
+
+Flag.propTypes = {
+  lang: PropTypes.string,
+};
+
+export default Flag;
