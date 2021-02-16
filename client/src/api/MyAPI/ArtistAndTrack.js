cafb6d623e9379d1dfa1ffef95e369a121e4869b
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-02-16 19:57:08 +0100

    Artists and tracks response to my api

---
+++ client/src/api/MyAPI/ArtistAndTrack.js
@@ -0,0 +1,32 @@
+import { LogoutUser, searchValue } from "../../utils/ApiUtils";
+import { spotifyApiActions } from "../../reducers/spotifyApiResponses";
+import axios from "axios";
+
+const GetArtistAndTrack = () => async (dispatch, getState) => {
+  const { search } = getState();
+  const queryString = `?search=${searchValue(search["searchText"])}`;
+  dispatch(spotifyApiActions.fetching());
+  await axios(`${process.env.REACT_APP_API_URL}/ArtistsAndTracks${queryString}`)
+    .then((response) => {
+      if (response.status === 200) {
+        const artists = response.data.artists.items.slice(0, 6);
+        dispatch(
+          spotifyApiActions.save({
+            artists: artists,
+            tracks: response.data.tracks.items,
+          })
+        );
+      } else {
+        LogoutUser(dispatch);
+        dispatch(spotifyApiActions.clear());
+        return undefined;
+      }
+    })
+    .catch(() => {
+      LogoutUser(dispatch);
+      dispatch(spotifyApiActions.clear());
+      return undefined;
+    });
+};
+
+export default GetArtistAndTrack;
