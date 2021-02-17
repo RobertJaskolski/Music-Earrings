73dd2a03777907ed8795302a2964a095f729169a
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-02-17 23:00:24 +0100

    Artist chip styles (desktop)

--- client/src/components/SearchResults/ArtistChip.js
+++ client/src/components/SearchResults/ArtistChip.js
@@ -1,34 +1,42 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Tooltip from "@material-ui/core/Tooltip";
+import QueueIcon from "@material-ui/icons/Queue";
 
 const Chip = styled.div`
   flex-basis: 95%;
-  cursor: pointer;
   border-radius: 5px;
-  margin-bottom: 25px;
+  margin-bottom: 35px;
   flex-wrap: wrap;
-  min-height: 35px;
+  min-height: 45px;
   background-color: #1db954;
+  display: flex;
+  justify-content: space-between;
   opacity: 0.8;
   transition-duration: 300ms;
   &:hover {
     opacity: 1;
+  }
+  svg {
+    font-size: 40px;
+    cursor: pointer;
   }
 `;
 
 const IMG = styled.img`
   position: absolute;
   margin-right: 5px;
+  border-radius: 4px 0px 4px 4px;
 `;
 
-const H3 = styled.h3`
-  margin: 0px 0px 0px 60px;
+const H2 = styled.h2`
+  margin: 0px 0px 0px 70px;
   padding: 0px;
 `;
 
 function ArtistChip(props) {
-  const { name, images } = props;
+  const { name, images, auth } = props;
   if (!name) {
     return null;
   }
@@ -36,12 +44,23 @@ function ArtistChip(props) {
     <Chip data-test='chipComponent'>
       <IMG
         data-test='chipIMG'
-        height='55px'
-        width='55px'
+        height='65px'
+        width='65px'
         src={images[0]?.url || "/images/wrapper.jpg"}
         alt={name}
       />
-      <H3 data-test='chipText'>{name}</H3>
+      <H2 data-test='chipText'>{name}</H2>
+      <span>
+        {auth ? (
+          <Tooltip title='Add to quequ'>
+            <QueueIcon />
+          </Tooltip>
+        ) : (
+          <Tooltip title='Add to quequ'>
+            <QueueIcon />
+          </Tooltip>
+        )}
+      </span>
     </Chip>
   );
 }
