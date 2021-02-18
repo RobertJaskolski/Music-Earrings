f4f8d80b4b5860c2ec796895e5479564b08a1b4c
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-02-18 15:25:18 +0100

    Refactor footer styles

--- client/src/components/Footer/style/style.js
+++ client/src/components/Footer/style/style.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+//Flag
 export const FlagBox = styled.div`
   width: 30px;
   height: 16px;
@@ -10,4 +11,27 @@ export const FlagBox = styled.div`
     opacity: 0.5;
     cursor: pointer;
   }
+`;
+
+//Footer
+export const FooterStyled = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 10px;
+`;
+
+export const Line = styled.div`
+  background-color: #1ed760;
+  height: 2px;
+  width: 100%;
+  margin-left: 6px;
+  margin-top: 10px;
+`;
+export const Ul = styled.span`
+  list-style-type: none;
+`;
+export const Li = styled.li`
+  display: inline-block;
+  margin: 15px;
 `;
