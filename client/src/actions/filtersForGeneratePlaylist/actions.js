6ebd36aa0a5532a7597deb0d938c881847d6bce3
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-02-19 18:07:33 +0100

    Added actions and Types for new reducer

---
+++ client/src/actions/filtersForGeneratePlaylist/actions.js
@@ -0,0 +1,16 @@
+import types from "./types";
+
+const addArtist = (artist) => ({ type: types.ADD_ARTIST, payload: artist });
+
+const deleteArtist = (artist) => ({
+  type: types.DELETE_ARTIST,
+  payload: artist,
+});
+
+const addTrack = (track) => ({ type: types.ADD_TRACK, payload: track });
+
+const deleteTrack = (track) => ({ type: types.DELETE_TRACK, payload: track });
+
+const clearFilters = () => ({ type: types.CLEAR_FILTERS });
+
+const ex = { addArtist, deleteArtist, addTrack, deleteTrack, clearFilters };
