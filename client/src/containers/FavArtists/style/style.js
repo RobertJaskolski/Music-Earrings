1bbeaeeefd9897fe36fa25c8190773c59abd3f6f
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-02-24 18:29:05 +0100

    Some css changes

--- client/src/containers/FavArtists/style/style.js
+++ client/src/containers/FavArtists/style/style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Div = styled.div`
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
+  justify-content: flex-start;
 `;
 
 export const H2 = styled.h2`
