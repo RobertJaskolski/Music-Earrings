6a737fa490a33e23c5a3b3cc2271e784289c790c
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-02-26 23:48:22 +0100

    Edit filter reducer

--- client/src/reducers/filtersForGeneratePlaylist/reducer.js
+++ client/src/reducers/filtersForGeneratePlaylist/reducer.js
@@ -3,9 +3,30 @@ import types from "../../actions/filtersForGeneratePlaylist/types";
 const INIT_STATE = {
   artistSeeds: [],
   trackSeeds: [],
+  limit: 25,
+  energy: [0, 100],
+  popularity: [0, 100],
+  danceable: [0, 100],
 };
 const filterReducer = (state = INIT_STATE, action) => {
   switch (action.type) {
+    case types.ADD_LIMIT:
+      return { ...state, limit: action.payload };
+    case types.DELETE_LIMIT:
+      return { ...state, limit: 25 };
+    case types.ADD_ENERGY:
+      return { ...state, energy: action.payload };
+    case types.DELETE_ENERGY:
+      return { ...state, energy: [100, 0] };
+    case types.ADD_POPULARITY:
+      return { ...state, popularity: action.payload };
+    case types.DELETE_POPULARITY:
+      return { ...state, popularity: [100, 0] };
+    case types.ADD_DANCEABLE:
+      return { ...state, danceable: action.payload };
+    case types.DELETE_DANCEABLE:
+      return { ...state, danceable: [100, 0] };
+
     case types.ADD_ARTIST:
       if (!state.artistSeeds.includes(action.payload))
         return {
