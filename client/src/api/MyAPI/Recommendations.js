7b19759f3a8400a453657f60c195df79a4ee7bbf
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-02-27 21:14:21 +0100

    Add api call for Recommendations My server

---
+++ client/src/api/MyAPI/Recommendations.js
@@ -0,0 +1,58 @@
+import { LogoutUser, searchValue } from "../../utils/ApiUtils";
+import { spotifyApiActions } from "../../reducers/spotifyApiResponses";
+import axios from "axios";
+
+const GetRecommendations = () => async (dispatch, getState) => {
+  const { tokens, filtrsGeneratePlaylist } = getState();
+  let seed = "";
+  if (filtrsGeneratePlaylist["artistSeeds"].length > 0) {
+    seed += "&seed_artists=";
+    filtrsGeneratePlaylist["artistSeeds"].map((item) => {
+      seed += item.id;
+      seed += ",";
+    });
+    seed = seed.slice(0, -1);
+  }
+  if (filtrsGeneratePlaylist["trackSeeds"].length > 0) {
+    seed += "&seed_tracks=";
+    filtrsGeneratePlaylist["trackSeeds"].map((item) => {
+      seed += item.id;
+      seed += ",";
+    });
+    seed = seed.slice(0, -1);
+  }
+  const filters = `limit=${filtrsGeneratePlaylist["limit"]}&min_popularity=${
+    filtrsGeneratePlaylist["popularity"][0]
+  }&max_popularity=${filtrsGeneratePlaylist["popularity"][1]}&min_energy=${
+    filtrsGeneratePlaylist["energy"][0] / 100
+  }&max_energy=${filtrsGeneratePlaylist["energy"][1] / 100}&min_danceability=${
+    filtrsGeneratePlaylist["danceable"][0] / 100
+  }&max_danceability=${filtrsGeneratePlaylist["danceable"][1] / 100}`;
+  const optionsAxios = options(
+    `/v1/recommendations?${filters}${seed}`,
+    tokens["accessToken"]
+  );
+  await axios(
+    `${process.env.REACT_APP_API_URL}/Recommendations/?${filters}${seed}`
+  )
+    .then((response) => {
+      if (response.status === 200) {
+        dispatch(
+          spotifyApiActions.saveTracklist({
+            tracklist: response.data,
+          })
+        );
+      } else {
+        LogoutUser(dispatch);
+        dispatch(spotifyApiActions.clearTracklist());
+        return undefined;
+      }
+    })
+    .catch((err) => {
+      LogoutUser(dispatch);
+      dispatch(spotifyApiActions.clearTracklist());
+      return undefined;
+    });
+};
+
+export default GetRecommendations;
--- client/src/api/MyAPI/index.js
+++ client/src/api/MyAPI/index.js
@@ -1,3 +1,4 @@
 import RefreshToken from "./RefreshToken";
 import GetArtistAndTrack from "./ArtistAndTrack";
+import GetRecommendations from "./Recommendations";
 export default { RefreshToken, GetArtistAndTrack };
