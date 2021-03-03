10182b17c3c90197b02d2eb291e74f277edd8b3c
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-03-03 21:58:20 +0100

    Fix height results

--- client/src/reducers/spotifyApiResponses/reducer.js
+++ client/src/reducers/spotifyApiResponses/reducer.js
@@ -9,9 +9,9 @@ const spotifyApiReducer = (state = INIT_STATE, action) => {
   switch (action.type) {
     case types.FETCHING_DATA:
       return { ...state, loading: true };
-    case types.CLEAR_DATA_TRACKLIST:
-      return { ...state, loading: false, tracks: [], artists: [] };
     case types.CLEAR_DATA_SEARCH:
+      return { ...state, loading: false, tracks: [], artists: [] };
+    case types.CLEAR_DATA_TRACKLIST:
       return { ...state, loading: false, tracklist: [] };
     case types.SAVE_RESPONSE_DATA_SEARCH:
       return {
