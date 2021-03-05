9c0b89fc693d16fb3def3452debd5b440433639a
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-03-05 23:43:33 +0100

    Added Tracks to tracklist

--- client/src/components/Tracklist/style/style.js
+++ client/src/components/Tracklist/style/style.js
@@ -33,8 +33,73 @@ export const Chip = styled.span`
     }
   }
 `;
+export const IMGTrack = styled.img`
+  position: absolute;
+`;
 export const Section = styled.section`
   margin-top: 15px;
   margin-left: 7px;
   margin-bottom: 5px;
+`;
+export const DivTrack = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  span {
+    flex-basis: 100%;
+  }
+  span:last-of-type {
+    margin-left: 45px;
+    display: flex;
+    justify-content: space-between;
+  }
+  svg {
+    font-size: 40px;
+    margin-right: 5px;
+    cursor: pointer;
+    transition: all linear 300ms;
+    :hover {
+      color: black;
+    }
+  }
+  img:last-of-type {
+    margin-right: 5px;
+  }
+`;
+export const LineTrack = styled.div`
+  width: 100%;
+  height: 1px;
+  margin-bottom: 5px;
+  background-color: #1db954;
+`;
+
+export const TextTrack = styled.span`
+  margin-left: 45px;
+`;
+
+export const ChipTrack = styled.div`
+  flex-basis: 95%;
+  width: 100%;
+  margin-bottom: 5px;
+  border-radius: 2px;
+  min-height: 40px;
+  opacity: 0.8;
+  transition-duration: 300ms;
+  span:first-child svg {
+    transition: all 0.3s linear;
+    opacity: 0;
+    position: absolute;
+    z-index: 10;
+    font-size: 40px;
+  }
+  :hover {
+    opacity: 1;
+    span:first-child svg {
+      opacity: 1;
+      cursor: pointer;
+      :hover {
+        color: black;
+      }
+    }
+  }
 `;
