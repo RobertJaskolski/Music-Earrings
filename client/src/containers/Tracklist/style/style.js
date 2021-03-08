d340531900e3c84d585b80ce67d2a36da78d4f70
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-03-08 20:52:31 +0100

    Edit styles for bacground

--- client/src/containers/Tracklist/style/style.js
+++ client/src/containers/Tracklist/style/style.js
@@ -3,6 +3,10 @@ export const Div = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: flex-start;
+  background-color: #141111;
+  padding: 15px;
+  border-radius: 3px;
+  box-shadow: 5px -5px rgba(20, 17, 17, 0.5);
 `;
 
 export const H2 = styled.h2`
