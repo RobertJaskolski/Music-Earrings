4185ead6770d3d263f4031873ea746cb0ae88749
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-03-09 20:43:12 +0100

    Add tracklist to spotify

--- client/src/api/SpotifyAPI/CreatePlaylist.js
+++ client/src/api/SpotifyAPI/CreatePlaylist.js
@@ -2,7 +2,12 @@ import { LogoutUser, optionsPOST } from "../../utils/ApiUtils";
 import axios from "axios";
 
 const CreatePlaylist = () => async (dispatch, getState) => {
-  const { filtrsGeneratePlaylist, userInfo, tokens } = getState();
+  const {
+    filtrsGeneratePlaylist,
+    userInfo,
+    tokens,
+    SpotifyResponses,
+  } = getState();
   const optionsAxios = optionsPOST(
     `/v1/users/${userInfo["data"].id}/playlists`,
     tokens["accessToken"],
@@ -14,7 +19,30 @@ const CreatePlaylist = () => async (dispatch, getState) => {
   );
   await axios(optionsAxios)
     .then((response) => {
-      if (response.status === 201) {
+      if (response.status === 201 || response.status === 200) {
+        let query = "";
+        SpotifyResponses["tracklist"]?.tracks.map((track) => {
+          query += track.uri + ",";
+          return track;
+        });
+        query = query.slice(0, -1);
+        const optionsAxios = optionsPOST(
+          `/v1/playlists/${response.data.id}/tracks?uris=${query}`,
+          tokens["accessToken"],
+          {}
+        );
+        axios(optionsAxios)
+          .then((response) => {
+            if (response.status === 201 || response.status === 200) {
+            } else {
+              LogoutUser(dispatch);
+              return undefined;
+            }
+          })
+          .catch((err) => {
+            LogoutUser(dispatch);
+            return undefined;
+          });
       } else {
         LogoutUser(dispatch);
         return undefined;
