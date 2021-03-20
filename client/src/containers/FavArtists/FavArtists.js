16a498ca21c07c63d83be055061420669c03ac15
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-03-20 19:33:52 +0100

    added alerts for adding artist from favs

--- client/src/containers/FavArtists/FavArtists.js
+++ client/src/containers/FavArtists/FavArtists.js
@@ -1,11 +1,17 @@
 // Import outside
-import React from "react";
+import React, { useState } from "react";
 import { Grid } from "@material-ui/core";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 // Import Components
 import FavArtist from "../../components/FavArtists/FavArtist";
 // Import Styles
 import { Div, H2, GridContainer } from "./style/style";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 function FavArtists({
   auth,
@@ -15,10 +21,22 @@ function FavArtists({
   addArtistToSeeds,
 }) {
   const changeFav = useMediaQuery("(min-width:500px)");
+  const [openSuccess, setOpenSuccess] = useState(false);
+  const [openFail, setOpenFail] = useState(false);
   const addToFilters = (item) => {
     if (trackSeeds.length + artistSeeds.length < 5) {
       addArtistToSeeds(item);
+      setOpenSuccess(true);
+    } else {
+      setOpenFail(true);
     }
+  };
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpenSuccess(false);
+    setOpenFail(false);
   };
   if (!auth) {
     return null;
@@ -40,6 +58,20 @@ function FavArtists({
           })}
         </Div>
       </Grid>
+      <Snackbar
+        open={openSuccess}
+        autoHideDuration={2000}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity="success">
+          The artist has been added!
+        </Alert>
+      </Snackbar>
+      <Snackbar open={openFail} autoHideDuration={3000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error">
+          You can generate a tracklist on the basis of 5 options!
+        </Alert>
+      </Snackbar>
     </GridContainer>
   );
 }
