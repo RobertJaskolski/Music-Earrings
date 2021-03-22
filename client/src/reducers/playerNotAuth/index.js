4ac3b6d11c547c8c1dde1244f17e7711b5f53016
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-03-22 21:59:45 +0100

    Added reducer for player not auth

---
+++ client/src/reducers/playerNotAuth/index.js
@@ -0,0 +1,4 @@
+import playerNotAuthReducer from "./reducer";
+export { default as playerNotAuthTypes } from "./types";
+export { default as playerNotAuthActions } from "./actions";
+export default playerNotAuthReducer;
