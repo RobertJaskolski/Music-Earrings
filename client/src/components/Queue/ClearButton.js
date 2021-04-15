e5a11a0933bc6c06496ea0d9669104bb9d2fd464
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-04-15 17:46:05 +0200

    Deleted queue components

--- client/src/components/Queue/ClearButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Button } from "../../styles/Buttons";
-
-function ClearButton(props) {
-  const { handleClearQueue } = props;
-  return <Button onClick={handleClearQueue}>Clear Queue</Button>;
-}
-
-export default ClearButton;
