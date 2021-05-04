8f16ac5b37f41a8c5eb9490cfe6a6fe74c85c8b7
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-05-04 14:04:54 +0200

    Created favArtists snapshot tests

--- client/src/__tests__/containers/FavArtists.unit.test.js
+++ client/src/__tests__/containers/FavArtists.unit.test.js
@@ -1,3 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import FavArtists from '../../containers/FavArtists/FavArtists';
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({ t: (key) => key }),
+}));
+
 describe('Fav artists container', () => {
-  test('should return', () => {});
+  test('should not render if user logout', () => {
+    const wrapper = shallow(<FavArtists auth={false} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+  test('should render correctly when user login', () => {
+    const wrapper = shallow(<FavArtists auth={true} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
 });
